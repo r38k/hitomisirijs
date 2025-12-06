a56347c11f5384315a476b74360b1b442dd1e3b2
r38k <lgmlgm6179@gmail.com>
2025-12-07 00:50:38 +0900

    Refactor code structure for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,13 +8,13 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://r38k.dev',
   integrations: [mdx(), sitemap()],
   adapter: cloudflare({
     platformProxy: {
       enabled: true
     },
 
-    imageService: "cloudflare"
+    imageService: "compile"
   }),
 });
